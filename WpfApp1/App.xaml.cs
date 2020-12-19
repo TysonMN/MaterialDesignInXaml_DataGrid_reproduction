95fed91b0718c1af57b205fdb03bde025152efc8
Tyson Williams <34664007+TysonMN@users.noreply.github.com>
2020-12-19 13:47:30 -0600

    Initial commit

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,5 @@
+﻿using System.Windows;
+
+namespace WpfApp1 {
+  public partial class App : Application { }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+
+namespace WpfApp1 {
+  public class MyItem {
+    public string MyString { get; set; }
+  }
+
+  public class ViewModel : INotifyPropertyChanged {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public string DataBindingSourceLevel => PresentationTraceSources.DataBindingSource.Switch.Level.ToString();
+
+    public void NotifyDataBindingSourceLevelChanged() =>
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DataBindingSourceLevel)));
+
+    public List<MyItem> MyItems { get; } = new List<MyItem> {
+      new MyItem { MyString = "A" },
+      new MyItem { MyString = "B" }
+    };
+
+    public static ViewModel DesignTimeViewModel { get; } = new ViewModel();
+  }
+
+  public class PopUpTraceListener : DefaultTraceListener {
+    public override void Write(string messagePart) => MessageBox.Show(messagePart);
+    public override void WriteLine(string messagePart) => MessageBox.Show(messagePart);
+  }
+
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      _ = PresentationTraceSources.DataBindingSource.Listeners.Add(new PopUpTraceListener());
+      DataContext = new ViewModel();
+      InitializeComponent();
+    }
+
+    private void OnClick(object _1, RoutedEventArgs _2) {
+      PresentationTraceSources.DataBindingSource.Switch.Level = SourceLevels.Warning;
+      ((ViewModel)DataContext).NotifyDataBindingSourceLevelChanged();
+    }
+  }
+}
